79b9a3f6d7a2505c7768fd4d2c54946834ece720
css <1805374493@qq.com>
2018-09-02 19:41:12 +0800

    "this is git init"

---
+++ src/main/java/com/baizhi/controller/EmpController.java
@@ -0,0 +1,20 @@
+package com.baizhi.controller;
+
+import com.baizhi.entity.Emp;
+import com.baizhi.mapper.EmpMapperss;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/emp")
+public class EmpController {
+    @Autowired
+    EmpMapperss empMapper;
+
+    @RequestMapping("/emp")
+    public Emp queryOne() {
+        return empMapper.selectByPrimaryKey(2);
+    }
+}
---
+++ src/main/java/com/baizhi/entity/Emp.java
@@ -0,0 +1,22 @@
+package com.baizhi.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Emp implements Serializable {
+    private Integer id;
+
+    private String name;
+
+    private Integer salary;
+
+    private Short age;
+
+
+}
